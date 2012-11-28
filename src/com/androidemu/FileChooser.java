b5d3f5477cf506c2994a4cf4fc6346c390877d28
AlexanderR <alexander.r@gmx.com>
2012-11-29 01:57:16 +1100

    Make FileChooser fully functional

--- src/com/androidemu/FileChooser.java
+++ src/com/androidemu/FileChooser.java
@@ -1,4 +1,4 @@
-package com.androidemu.nes;
+package com.androidemu;
 
 import android.app.ListActivity;
 import android.content.ActivityNotFoundException;
@@ -20,17 +20,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class FileChooser extends ListActivity implements
-		FileFilter, View.OnClickListener, View.OnKeyListener {
+public class FileChooser extends ListActivity implements FileFilter,
+		View.OnClickListener, View.OnKeyListener
+{
 
 	public static final String EXTRA_TITLE = "title";
 	public static final String EXTRA_FILTERS = "filters";
-
-	private static final String ROM_GRIPPER_PACKAGE = "com.bingo.rom_gripper";
-	private static final String ROM_GRIPPER_ACTION = ROM_GRIPPER_PACKAGE +
-			".action.ROM_LIST";
-	private static final Uri ROM_GRIPPER_URI = Uri.parse(
-			"http://market.android.com/details?id=" + ROM_GRIPPER_PACKAGE);
 
 	private static final String LOG_TAG = "FileChooser";
 	private final File sdcardDir = new File("/sdcard");
@@ -39,7 +34,8 @@ public class FileChooser extends ListActivity implements
 	private EditText pathEdit;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.file_chooser);
@@ -48,13 +44,11 @@ public class FileChooser extends ListActivity implements
 		pathEdit = (EditText) findViewById(R.id.path);
 		pathEdit.setOnKeyListener(this);
 
-		findViewById(R.id.rom_gripper).setOnClickListener(this);
 		findViewById(R.id.goto_sdcard).setOnClickListener(this);
 		findViewById(R.id.goto_parent).setOnClickListener(this);
 
 		String title = getIntent().getStringExtra(EXTRA_TITLE);
-		if (title != null)
-			setTitle(title);
+		if (title != null) setTitle(title);
 		filters = getFileFilter();
 
 		String path = null;
@@ -64,15 +58,14 @@ public class FileChooser extends ListActivity implements
 			path = getInitialPath();
 
 		File dir = null;
-		if (path != null)
-			dir = getDirectoryFromFile(path);
-		if (dir == null)
-			dir = sdcardDir;
+		if (path != null) dir = getDirectoryFromFile(path);
+		if (dir == null) dir = sdcardDir;
 		changeTo(dir);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		super.onCreateOptionsMenu(menu);
 
 		getMenuInflater().inflate(R.menu.file_chooser, menu);
@@ -80,17 +73,19 @@ public class FileChooser extends ListActivity implements
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.menu_refresh:
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		if (item.getItemId() == R.id.menu_refresh)
+		{
 			changeTo(currentDir);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	@Override
-	protected void onSaveInstanceState(Bundle outState) {
+	protected void onSaveInstanceState(Bundle outState)
+	{
 		super.onSaveInstanceState(outState);
 
 		if (currentDir != null)
@@ -98,7 +93,8 @@ public class FileChooser extends ListActivity implements
 	}
 
 	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
+	protected void onListItemClick(ListView l, View v, int position, long id)
+	{
 		String name = l.getItemAtPosition(position).toString();
 		File f = new File(currentDir, name);
 		if (f.isDirectory())
@@ -107,32 +103,37 @@ public class FileChooser extends ListActivity implements
 			onFileSelected(Uri.fromFile(f));
 	}
 
-	protected String[] getFileFilter() {
+	protected String[] getFileFilter()
+	{
 		return getIntent().getStringArrayExtra(EXTRA_FILTERS);
 	}
 
-	protected String getInitialPath() {
+	protected String getInitialPath()
+	{
 		Uri uri = getIntent().getData();
-		if (uri == null)
-			return null;
+		if (uri == null) return null;
 		return uri.getPath();
 	}
 
-	protected void onFileSelected(Uri uri) {
+	protected void onFileSelected(Uri uri)
+	{
 		setResult(RESULT_OK, new Intent().setData(uri));
 		finish();
 	}
 
-	public boolean onKey(View v, int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_ENTER) {
+	public boolean onKey(View v, int keyCode, KeyEvent event)
+	{
+		if (keyCode == KeyEvent.KEYCODE_ENTER)
+		{
 			String name = pathEdit.getText().toString().trim();
-			if (name.length() > 0) {
+			if (name.length() > 0)
+			{
 				File dir = new File(name);
 				if (dir.isDirectory())
 					changeTo(dir);
-				else {
-					Toast.makeText(this, R.string.invalid_dir,
-							Toast.LENGTH_SHORT).show();
+				else
+				{
+					Toast.makeText(this, R.string.invalid_dir, Toast.LENGTH_SHORT).show();
 				}
 				return true;
 			}
@@ -140,67 +141,65 @@ public class FileChooser extends ListActivity implements
 		return false;
 	}
 
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.goto_sdcard:
+	public void onClick(View v)
+	{
+		if (v.getId() == R.id.goto_sdcard)
+		{
 			changeTo(sdcardDir);
-			break;
-
-		case R.id.goto_parent:
+		}
+		else if (v.getId() == R.id.goto_parent)
+		{
 			File parent = currentDir.getParentFile();
-			if (parent != null)
-				changeTo(parent);
-			break;
+			if (parent != null) changeTo(parent);
 		}
 	}
 
-	public boolean accept(File file) {
+	public boolean accept(File file)
+	{
 		String name = file.getName();
 
 		// Do not show hidden files
-		if (name.startsWith("."))
-			return false;
+		if (name.startsWith(".")) return false;
 
 		// Always show directory
-		if (file.isDirectory())
-			return true;
+		if (file.isDirectory()) return true;
 
 		name = name.toLowerCase();
-		for (String f : filters) {
-			if (name.endsWith(f))
-				return true;
+		for (String f : filters)
+		{
+			if (name.endsWith(f)) return true;
 		}
 		return false;
 	}
 
-	private File getDirectoryFromFile(String path) {
+	private File getDirectoryFromFile(String path)
+	{
 		File dir = new File(path);
-		if (!dir.isDirectory()) {
+		if (!dir.isDirectory())
+		{
 			dir = dir.getParentFile();
-			if (dir != null && !dir.isDirectory())
-				dir = null;
+			if (dir != null && !dir.isDirectory()) dir = null;
 		}
 		return dir;
 	}
 
-	private void changeTo(File dir) {
+	private void changeTo(File dir)
+	{
 		File[] files = dir.listFiles(filters == null ? null : this);
-		if (files == null)
-			files = new File[0];
+		if (files == null) files = new File[0];
 
 		currentDir = dir;
 		pathEdit.setText(dir.getAbsolutePath());
 
 		List<String> items = new ArrayList<String>(files.length);
-		for (File f : files) {
+		for (File f : files)
+		{
 			String name = f.getName();
-			if (f.isDirectory())
-				name += '/';
+			if (f.isDirectory()) name += '/';
 			items.add(name);
 		}
 
 		Collections.sort(items, String.CASE_INSENSITIVE_ORDER);
-		setListAdapter(new ArrayAdapter(this,
-				android.R.layout.simple_list_item_1, items));
+		setListAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, items));
 	}
 }
